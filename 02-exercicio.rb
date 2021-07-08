5b82922bc5800464b08a598d234b060747513753
Natalia Floridi <nataliafloridi@gmail.com>
2021-07-08 18:10:54 -0300

    modified 02-exercicio.rb

--- 02-exercicio.rb
+++ 02-exercicio.rb
@@ -1,11 +1,10 @@
 puts "Escreva um programa que calcula e exiba na tela:"
 
 segundos = 60
-minutos = 60
-hora  = 60
-dia = 24
-um_ano = 12
-ano = 365
+minuto_em_segundos = 60
+dia_em_horas = 24
+ano_em_meses = 12
+ano_em_dias = 365
 decada = 10
 idade_natalia = 34
 idade = 1232000000.0
@@ -13,18 +12,18 @@ expectativa_de_vida = 80
 chocolate = 5
 
 puts "- quantas horas há em um ano?"
-puts dia * ano
+puts dia_em_horas * ano_em_dias
 puts
 puts "- quantos minutos há em uma década?"
-puts minutos * dia * ano * decada
+puts minuto_em_segundos * dia_em_horas * ano_em_dias * decada
 puts
 puts "- qual é a sua idade em segundos?"
-puts segundos * minutos * dia * ano * idade_natalia
+puts segundos * minuto_em_segundos * dia_em_horas * ano_em_dias * idade_natalia
 puts
 puts "- quantos chocolates você pretende comer na vida?"
-puts chocolate * um_ano * expectativa_de_vida
+puts chocolate * ano_em_meses * expectativa_de_vida
 puts
 puts "Desafio:"
 puts
 puts "- Se minha idade é de 1232 milhões de segundos, qual é minha idade em anos?"
-puts idade / (segundos * minutos * dia * ano)
+puts idade / (segundos * minuto_em_segundos * dia_em_horas * ano_em_dias)
