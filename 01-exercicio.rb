af999ff502a16fc83e18dbaae3691127ba0332ec
Natalia Floridi <nataliafloridi@gmail.com>
2021-07-08 15:23:06 -0300

    new file 01-exercicio.rb

---
+++ 01-exercicio.rb
@@ -0,0 +1,20 @@
+puts "Escreva um programa que calcula e exiba na tela:"
+puts
+puts "- quantas horas há em um ano?"
+puts 24 * 365
+puts
+puts "- quantos minutos há em uma década?"
+puts 8760 * 60 * 10
+puts (60 * 24) * (365 * 10)
+puts
+puts "- qual é a sua idade em segundos?"
+puts (5256000 * 34 / 10) * 60
+puts ((60 * 60) * 24) * (365 * 34)
+puts
+puts "- quantos chocolates você pretende comer na vida?"
+puts 5 * 12 * 80
+puts
+puts "Desafio:"
+puts
+puts "- Se minha idade é de 1232 milhões de segundos, qual é minha idade em anos?"
+puts 1232000000 / (3600 * 24 * 365)
