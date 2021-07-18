17a6e25bb08026a67ff4db64e44649d82e83752e
Natalia Floridi <nataliafloridi@gmail.com>
2021-07-18 11:23:40 -0300

    modified files

--- 10-exercicio-parte-1.rb
+++ 10-exercicio-parte-1.rb
@@ -1,23 +1,26 @@
 puts "1) Desenhe um quadrado de 10 linhas por 10 colunas. Desenhar as bordas com “*”, preencher com “x”."
 
+# primeira linha 
 for a in (1..10)
     print "*"
 end
 puts 
 
+# primeria coluna 
 for b in (1..8) 
     print "*"
-
+    # linhas internas
     for c in (1..8)
         print "x" 
     end
-    
+    # ultima coluna
     for d in (10..10)
         print "*"
     end  
     puts
 end
 
+# ultima linha
 for e in (1..10)
     print "*"
 end    
--- 10-exercicio-parte-2.rb
+++ 10-exercicio-parte-2.rb
@@ -1,11 +1,23 @@
-2) Esse aqui é um desafio. Faça um código para obter esse resultado:
-        *
-        **
-        ***
-        ****
-        *****
-        ******
-        *******
-        ********
-        *********
-        **********
+# 2) Esse aqui é um desafio. Faça um código para obter esse resultado:
+# *
+# **
+# ***
+# ****
+# *****
+# ******
+# *******
+# ********
+# *********
+# **********
+
+# para cada linha, adiciona mais 1 "*" na coluna até que chegue ao número total de linhas.
+
+for linha in (1..10)
+    print "*"
+  
+    for coluna in (1...linha)
+        print "*"
+    end
+    
+    puts
+end 
