55a05b5ed5a5c1c005f057339ad7417395fb9928
Natalia Floridi <nataliafloridi@gmail.com>
2021-07-21 17:48:31 -0300

    new file

--- simulado/05-simulado.rb
+++ simulado/05-simulado.rb
@@ -1,7 +1,25 @@
-5) Faça uma função chamada 'mediana' que recebe um array de números e retorna a mediana entre eles.
-Lembrando que para obter a mediana de uma série de números, é necessário ordenar a série (usar a função sort()) e retornar:
-- se a série tiver um número (n) ímpar de elementos, retornar o número da posição n/2 (a parte inteira);
-- e caso se a série tiver um número (n) par de elementos, retornar a média entre os números das posições (n/2)-1 e (n/2)
-Ex.:
-puts(mediana([10, 0, -1, -500, 20, 100])) # deve imprimir 5.0
-puts(mediana([10, 0, -1, -500, 20]))      # deve imprimir 0.0
+# 5) Faça uma função chamada 'mediana' que recebe um array de números e retorna a mediana entre eles.
+# Lembrando que para obter a mediana de uma série de números, é necessário ordenar a série (usar a função sort()) e retornar:
+# - se a série tiver um número (n) ímpar de elementos, retornar o número da posição n/2 (a parte inteira);
+# - e caso se a série tiver um número (n) par de elementos, retornar a média entre os números das posições (n/2)-1 e (n/2)
+# Ex.:
+
+array = [10, 0, -1, -500, 20, 100]
+
+def mediana(array)
+    array_ordenado = array.sort()
+    return array_ordenado
+
+    for n in (0..array_ordenado.size-1)
+        valor = array_ordenado[n] % 2
+
+        if valor == 0
+            print "par"
+        else
+            print "impar"
+        end        
+    end
+end
+
+# puts(mediana([10, 0, -1, -500, 20, 100])) # deve imprimir 5.0
+# puts(mediana([10, 0, -1, -500, 20]))      # deve imprimir 0.0
--- simulado/06-simulado.rb
+++ simulado/06-simulado.rb
@@ -1,4 +1,25 @@
-6) Faça uma função chamada 'menor_maior' que recebe um array de números e que deve retornar outro array, contendo apenas 2 elementos: o primeiro deve ser o menor número do array informado e o segundo deve ser o maior.
-    Ex.:
-    print(menor_maior([10, 0, -1, -500, 20, 100])) # deve imprimir [-500, 100]
-    puts
+# 6) Faça uma função chamada 'menor_maior' que recebe um array de números e que deve retornar outro array, contendo apenas 2 elementos: o primeiro deve ser o menor número do array informado e o segundo deve ser o maior.
+
+def menor_maior(array)
+    novo_array = []
+
+    for n in (0...array.size)
+        if n == 0
+            menor = array[n]
+            maior = array[n]
+        else
+            if array[n] < menor
+                menor = array[n]
+            elsif array[n] > maior  
+                maior = array[n]
+            end
+        end
+    end    
+
+    novo_array[1] = menor
+    novo_array[2] = maior
+    return novo_array
+end
+
+print(menor_maior([10, 0, -1, -500, 20, 100])) # deve imprimir [-500, 100]
+puts
